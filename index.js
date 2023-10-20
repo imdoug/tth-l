14c368e896137b73c1d8ced562edb482e8498591
imdoug <imdoug@icloud.com>
2023-10-20 16:10:10 -0400

    last looks 7

--- index.js
+++ index.js
@@ -29,84 +29,52 @@ io.on("connect", (socket)=>{
 
       //TEST A LISTENER FOR EACH PART TO CHECK HOW LONG IT TAKES 
       socket.on("r1", (data)=>{
-            let obj = {
-                  commitment: "Top Left",
-                  timeStamp : data.timeStamp
-            }
-            // jsonReader('./db.json',(err, data)=>{
-            //       if(err){
-            //             console.log(err)
-            //       }else{
-            //             data.clicks.push(obj)
-            //             fs.writeFile('./db.json', JSON.stringify(data), err =>{
-            //                   if(err){
-            //                         err
-            //                   }
-            //             })
-            //       }
-            // })
+            let obj = data;
+            jsonReader('./db.json',(err, data)=>{
+                  if(err){
+                        console.log(err)
+                  }else{
+                        data.clicks.states.push(obj)
+                        fs.writeFile('./db.json', JSON.stringify(data), err =>{
+                              if(err){
+                                    console.log(err)
+                              }
+                        })
+                  }
+            })
             console.log(data)
-            io.emit("r1",{key: 11, round: data.round})
       })
       socket.on("r2", (data)=>{
-            let obj = {
-                  commitment: "Top Right",
-                  timeStamp : data.timeStamp
-            }
-            // jsonReader('./db.json',(err, data)=>{
-            //       if(err){
-            //             console.log(err)
-            //       }else{
-            //             data.clicks.push(obj)
-            //             fs.writeFile('./db.json', JSON.stringify(data), err =>{
-            //                   if(err){
-            //                         err
-            //                   }
-            //             })
-            //       }
-            // })
+            let obj = data;
+            jsonReader('./db.json',(err, data)=>{
+                  if(err){
+                        console.log(err)
+                  }else{
+                        data.clicks.popUps.push(obj)
+                        fs.writeFile('./db.json', JSON.stringify(data), err =>{
+                              if(err){
+                                    console.log(err)
+                              }
+                        })
+                  }
+            })
             console.log(data)
-            io.emit("r2",{key: 22,round: data.round})
       })
       socket.on("r3", (data)=>{
-            let obj = {
-                  commitment: "Bottom Left",
-                  timeStamp : data.timeStamp
-            }
-            // jsonReader('./db.json',(err, data)=>{
-            //       if(err){
-            //             console.log(err)
-            //       }else{
-            //             data.clicks.push(obj)
-            //             fs.writeFile('./db.json', JSON.stringify(data), err =>{
-            //                   if(err){
-            //                         err
-            //                   }
-            //             })
-            //       }
-            // })
+            let obj = data;
+            jsonReader('./db.json',(err, data)=>{
+                  if(err){
+                        console.log(err)
+                  }else{
+                        data.clicks.pages.push(obj)
+                        fs.writeFile('./db.json', JSON.stringify(data), err =>{
+                              if(err){
+                                    console.log(err)
+                              }
+                        })
+                  }
+            })
             console.log(data)
-            io.emit("r3",{key: 33,round: data.round})
-      })
-      socket.on("r4", (data)=>{
-            let obj = {
-                  commitment: "BottomRight",
-                  timeStamp : data.timeStamp
-            }
-            // jsonReader('./db.json',(err, data)=>{
-            //       if(err){
-            //             console.log(err)
-            //       }else{
-            //             data.clicks.push(obj)
-            //             fs.writeFile('./db.json', JSON.stringify(data), err =>{
-            //                   if(err){
-            //                         err
-            //                   }
-            //             })
-            //       }
-            // })
-            console.log(data)
-            io.emit("r4",{key: 44,round: data.round})
       })
       console.log("a user connected")
 
